0940aedaa9f8f01690eca000316dd502bf6f0a03
Kapila Jain <kapila@grubmenow.com>
2014-09-22 00:02:54 -0700

    adding data import sql queries

---
+++ grubmenow-webapp/configuration/sql/data/foodItems.sql
@@ -0,0 +1,29 @@
+insert into FOOD_ITEM (FOOD_ITEM_ID, FOOD_ITEM_NAME, FOOD_ITEM_IMAGE_URL, FOOD_ITEM_DESCRIPTION, 
+FOOD_ITEM_DESCRIPTION_TAGS, FOOD_ITEM_STATE)
+values
+('645637', 'Paneer Tikka Masala', 'http://bombaygrilltx.com/wp-content/uploads/2013/09/Paneer-Tikka-Masala.png',
+'Grilled cottage cheese cubes in tomato sauce.', null, 'ACTIVE');
+
+insert into FOOD_ITEM (FOOD_ITEM_ID, FOOD_ITEM_NAME, FOOD_ITEM_IMAGE_URL, 
+FOOD_ITEM_DESCRIPTION, FOOD_ITEM_DESCRIPTION_TAGS, FOOD_ITEM_STATE)
+values
+('245633', 'Masala Dosa', 'http://masalacook.typepad.com/.a/6a01053695fa9d970b0120a5e5d305970c-800wi',
+'Thin rice crepes stuffed with spicy potatoes.', null, 'ACTIVE');
+
+insert into FOOD_ITEM (FOOD_ITEM_ID, FOOD_ITEM_NAME, FOOD_ITEM_IMAGE_URL, 
+FOOD_ITEM_DESCRIPTION, FOOD_ITEM_DESCRIPTION_TAGS, FOOD_ITEM_STATE)
+values
+('225636', 'Paneer Tikka Masala', 'http://bombaygrilltx.com/wp-content/uploads/2013/09/Paneer-Tikka-Masala.png',
+'Grilled cottage cheese cubes in tomato sauce.', null, 'ACTIVE');
+
+insert into FOOD_ITEM (FOOD_ITEM_ID, FOOD_ITEM_NAME, FOOD_ITEM_IMAGE_URL, 
+FOOD_ITEM_DESCRIPTION, FOOD_ITEM_DESCRIPTION_TAGS, FOOD_ITEM_STATE)
+values
+('475630','Madras Masala Dosa', 'http://masalacook.typepad.com/.a/6a01053695fa9d970b0120a5e5d305970c-800wi', 
+'Thin rice crepes stuffed with spicy potatoes.', null, 'ACTIVE');
+
+insert into FOOD_ITEM (FOOD_ITEM_ID, FOOD_ITEM_NAME, FOOD_ITEM_IMAGE_URL, 
+FOOD_ITEM_DESCRIPTION, FOOD_ITEM_DESCRIPTION_TAGS, FOOD_ITEM_STATE)
+values
+('885637', 'Paneer Tikka Masala2' , 'http://bombaygrilltx.com/wp-content/uploads/2013/09/Paneer-Tikka-Masala.png',
+'Grilled cottage cheese cubes in tomato sauce.', null, 'ACTIVE');
