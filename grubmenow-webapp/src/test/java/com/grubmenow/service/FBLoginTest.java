aca445c18fc9f6a49ce10b62d109734b8a9309d0
Kapila Jain <kapila@grubmenow.com>
2014-10-03 00:48:02 -0700

    Forgot to checkin test case fix

--- grubmenow-webapp/src/test/java/com/grubmenow/service/FBLoginTest.java
+++ grubmenow-webapp/src/test/java/com/grubmenow/service/FBLoginTest.java
@@ -1,11 +1,12 @@
 package com.grubmenow.service;
 
-import com.grubmenow.service.fbauth.FBAuth;
+import com.grubmenow.service.auth.FacebookAuthentication;
+
 
 public class FBLoginTest {
 	
 	public static void main(String[] args) {
-		FBAuth fbAuth = new FBAuth("730291313693447", "c313676be38b8efe9baaf9b8833d3db5");
+		FacebookAuthentication fbAuth = new FacebookAuthentication("730291313693447", "c313676be38b8efe9baaf9b8833d3db5");
 		
 		String accessToken = "CAAKYMjJV0wcBAJ7kUJEXPo3kTueFZBBxcwkwPqcCjen73BXJFWrXOfdPGLCExBZBplh9bpwtSixnHMZAswvwKjt2gznR3OjnianSYSSN4PJlW4lU0irjqlurPXwyLZA52qSeXxcH6swCFUiSZBtz95jUYYP2Lr0wfJmhRIKnXyiZAcCk2EFTzSQVltuxAlo3wjHcu3lWnZCQgOp7SmOhJGZAgJBn9ChHM9EZD";
 		boolean isConnected = fbAuth.isAuthenticated(accessToken);
